82f578e4438f5df6565a213323a5b7e51585a1fa
yosel <yosel11517@gmail.com>
2026-07-23 17:22:55 -0600

    Update compileSdk to 37 and upgrade dependencies across the project y se agrega material 3 expressive

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "yosel.dev.facturascan"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
