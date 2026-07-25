5b36c213187e904c85e069bdc72bf931b3efcab5
yosel <yosel11517@gmail.com>
2026-07-24 19:00:05 -0600

    Refactor Navigation 3 graph by moving screen entries into separate extension functions in EntryScreens.kt

--- app/src/main/java/yosel/dev/facturascan/core/navigation/AppNavigation.kt
+++ app/src/main/java/yosel/dev/facturascan/core/navigation/AppNavigation.kt
@@ -1,12 +1,6 @@
 package yosel.dev.facturascan.core.navigation
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.ui.NavDisplay
@@ -24,45 +18,23 @@ fun AppNavigation(startDestination: Screens) {
         },
         entryProvider = entryProvider {
 
-            entry<Screens.MyBills> {
-                Scaffold() { paddingValues ->
-                    Column(
-                        modifier = Modifier.padding(paddingValues)
-                    ) {
-                        Text(text = "My Bills")
-                        Button(onClick = { backStack.add(Screens.UploadBill) }) {
-                            Text(text = "Upload Bill")
-                        }
-
-                    }
+            myBillsEntry(
+                onNavigate = { screen ->
+                    backStack.add(screen)
                 }
-            }
+            )
 
-            entry<Screens.UploadBill> {
-                Scaffold() { paddingValues ->
-                    Column(
-                        modifier = Modifier.padding(paddingValues)
-                    ) {
-                        Text(text = "Upload Bill")
-                        Button(onClick = { backStack.add(Screens.DetailBill) }) {
-                            Text(text = "Detail Bill")
-                        }
-                    }
+            uploadBillEntry(
+                onNavigate = { screen ->
+                    backStack.add(screen)
                 }
-            }
+            )
 
-            entry<Screens.DetailBill> {
-                Scaffold() { paddingValues ->
-                    Column(
-                        modifier = Modifier.padding(paddingValues)
-                    ) {
-                        Text(text = "Detail Bill")
-                        Button(onClick = { backStack.removeLastOrNull() }) {
-                            Text(text = "back")
-                        }
-                    }
+            detailBillEntry(
+                onBack = {
+                    backStack.removeLastOrNull()
                 }
-            }
+            )
         }
     )
 }
