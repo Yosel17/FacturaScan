ef247761437808cad16ddf280c4014542e961372
Yosel Alvizures <72110302+Yosel17@users.noreply.github.com>
2026-07-25 16:25:27 -0600

    My bills screen functionality
    * Implement MyBillsScreen and add SnackBarError shared component
    
    * Add Material Icons Extended and implement TopBarGlobal component in MyBillsScreen
    
    * Implement MyBills UI components and update screen with FAB and loading state
    
    * Integrate MyBillsViewModel into navigation and update MyBillsScreen with loading state and previews

--- app/src/main/java/yosel/dev/facturascan/core/navigation/EntryScreens.kt
+++ app/src/main/java/yosel/dev/facturascan/core/navigation/EntryScreens.kt
@@ -1,28 +1,39 @@
 package yosel.dev.facturascan.core.navigation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
+import yosel.dev.facturascan.screens.my_bills.ui.MyBillsScreen
+import yosel.dev.facturascan.screens.my_bills.ui.MyBillsViewModel
 
 fun EntryProviderScope<NavKey>.myBillsEntry(
     onNavigate: (Screens) -> Unit
 ) {
     entry<Screens.MyBills> {
-        Scaffold { paddingValues ->
-            Column(
-                modifier = Modifier.padding(paddingValues)
-            ) {
-                Text(text = "My Bills")
-                Button(onClick = { onNavigate(Screens.UploadBill) }) {
-                    Text(text = "Upload Bill")
-                }
-            }
-        }
+        val viewmodel = hiltViewModel<MyBillsViewModel>()
+        val state by viewmodel.state.collectAsStateWithLifecycle()
+        val snackBarHostState = remember { SnackbarHostState() }
+
+        MyBillsScreen(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background),
+            state = state,
+            snackBarHostState = snackBarHostState
+        )
     }
 }
 
---
+++ app/src/main/java/yosel/dev/facturascan/screens/my_bills/ui/MyBillsScreen.kt
@@ -0,0 +1,99 @@
+package yosel.dev.facturascan.screens.my_bills.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Photo
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LoadingIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import yosel.dev.facturascan.core.components.SnackBarError
+import yosel.dev.facturascan.core.components.TopBarGlobal
+import yosel.dev.facturascan.ui.theme.FacturaScanTheme
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+fun MyBillsScreen(
+    modifier: Modifier = Modifier,
+    state: MyBillsState,
+    snackBarHostState: SnackbarHostState
+) {
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = {
+            SnackbarHost(hostState = snackBarHostState){ data ->
+                SnackBarError(data = data)
+            }
+        },
+        topBar = {
+            TopBarGlobal(
+                title = "FacturaScan"
+            )
+        },
+        floatingActionButton = {
+            if (!state.isLoading){
+                ExtendedFloatingActionButton(
+                    onClick = {},
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Filled.Photo,
+                            contentDescription = "Photo"
+                        )
+                    },
+                    text = { Text(text = "Escanear Factura") },
+                    expanded = true
+                )
+            }
+        }
+    ) { paddingValues ->
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ){
+            when{
+                state.isLoading ->{
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        LoadingIndicator()
+                    }
+                }
+                else ->{
+                    BodyMyBills(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(horizontal = 24.dp),
+                        state = state,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun Screen() {
+    FacturaScanTheme{
+        MyBillsScreen(
+            modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background),
+            state = MyBillsState(
+                isLoading = true
+            ),
+            snackBarHostState = SnackbarHostState()
+        )
+    }
+}
