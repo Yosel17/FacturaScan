028de43e1b34b66d290e4052a747e88fa09a216c
yosel <yosel11517@gmail.com>
2026-07-25 15:56:26 -0600

    Implement MyBills UI components and update screen with FAB and loading state

---
+++ app/src/main/java/yosel/dev/facturascan/screens/my_bills/ui/MyBillsComp.kt
@@ -0,0 +1,15 @@
+package yosel.dev.facturascan.screens.my_bills.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BodyMyBills(
+    modifier: Modifier = Modifier,
+    state: MyBillsState
+) {
+    Column(
+        modifier = modifier
+    ) { }
+}
--- app/src/main/java/yosel/dev/facturascan/screens/my_bills/ui/MyBillsScreen.kt
+++ app/src/main/java/yosel/dev/facturascan/screens/my_bills/ui/MyBillsScreen.kt
@@ -1,13 +1,26 @@
 package yosel.dev.facturascan.screens.my_bills.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Photo
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LoadingIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import yosel.dev.facturascan.core.components.SnackBarError
 import yosel.dev.facturascan.core.components.TopBarGlobal
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun MyBillsScreen(
     modifier: Modifier = Modifier,
@@ -25,6 +38,42 @@ fun MyBillsScreen(
             TopBarGlobal(
                 title = "FacturaScan"
             )
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = {},
+                icon = {
+                    Icon(
+                        imageVector = Icons.Filled.Photo,
+                        contentDescription = "Photo"
+                    )
+                },
+                text = { Text(text = "Escanear Factura") },
+                expanded = true
+            )
         }
-    ) { }
+    ) { paddingValues ->
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ){
+            when{
+                state.isLoading ->{
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        LoadingIndicator()
+                    }
+                }
+                else ->{
+                    BodyMyBills(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(horizontal = 24.dp),
+                        state = state,
+                    )
+                }
+            }
+        }
+    }
 }
