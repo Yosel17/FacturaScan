2074c74d7825b95a6d180420d07e1f4f38cd42a0
yosel <yosel11517@gmail.com>
2026-07-25 11:42:16 -0600

    Add BillResponse model and implement BillsDataSource to fetch documents from Firestore

---
+++ app/src/main/java/yosel/dev/facturascan/core/data_source/BillsDataSource.kt
@@ -0,0 +1,20 @@
+package yosel.dev.facturascan.core.data_source
+
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+import yosel.dev.facturascan.core.model.response.BillResponse
+import yosel.dev.facturascan.core.utils.Constants
+import javax.inject.Inject
+
+class BillsDataSource @Inject constructor(
+    private val firestore: FirebaseFirestore
+) {
+
+    suspend fun getAllBills(): List<BillResponse> {
+        return firestore.collection(Constants.BILLS_COLLECTION)
+            .get()
+            .await()
+            .toObjects(BillResponse::class.java)
+    }
+
+}
