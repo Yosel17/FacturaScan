df6700fde4bcef6ccd66c3d2ef2c371b00810982
yosel <yosel11517@gmail.com>
2026-07-29 12:13:05 -0600

    Configure Room database, implement Bill entity and DAO, and add Hilt DI module

---
+++ app/src/main/java/yosel/dev/facturascan/core/room/tables/bill/BillEntity.kt
@@ -0,0 +1,21 @@
+package yosel.dev.facturascan.core.room.tables.bill
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "bills")
+data class BillEntity(
+    @PrimaryKey
+    val id: String,
+    val createdAt: Long,
+    val companyName: String,
+    val vendorTaxId: String,
+    val customerTaxId: String,
+    val invoiceNumber: String,
+    val serialNumber: String,
+    val authorizationNumber: String,
+    val issueDate: String,
+    val totalAmount: Double,
+    val description: String,
+    val imageUrl: String
+)
