a9a9ef51ac55f55abf489d50bd88575bbb82b9ba
yosel <yosel11517@gmail.com>
2026-07-31 18:56:12 -0600

    Add AndroidX Core Splashscreen dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
     ksp(libs.room.compiler)
+    implementation(libs.core.splashscreen)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
